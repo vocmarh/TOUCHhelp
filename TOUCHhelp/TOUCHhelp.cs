bc9d3822293ba450475ae2a51eadc59aadddba12
Сергей Храмцов <vocmarh@mail.ru>
2022-05-13 17:08:45 +0500

    Добавьте файлы проекта.

---
+++ TOUCHhelp/TOUCHhelp.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOUCHhelp
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;           
+
+            TaskDialog.Show("ИНФО", "Данный плагин разработан компанией TOUCH, который позволяет подсчитывать площадь и объем выбранных стен");
+
+            return Result.Succeeded;
+        }
+    }
+}
